1f7c9ff8d1a6528160e8f81574b97d2575c09d61
blue-anvil <blue_anvil@icloud.com>
2026-05-14 06:33:53 -0500

    parallel downloads for hashing

--- generate_hashes.py
+++ generate_hashes.py
@@ -1,32 +1,40 @@
 import boto3
 import argparse
 import hashlib
+from concurrent.futures import ThreadPoolExecutor
 
 parser = argparse.ArgumentParser(add_help=False)
 parser.add_argument('-b', '--bucket', required=True)
 parser.add_argument('-p', '--prefix', default='')
 parser.add_argument('-f', '--file', required=True)
 parser.add_argument('-t', '--test', action='store_true', default=False)
+parser.add_argument('-w', '--workers', type=int, default=10)
 args = parser.parse_args()
 
 s3 = boto3.client('s3')
 paginator = s3.get_paginator('list_objects_v2')
 
-lines = []
-for page in paginator.paginate(Bucket=args.bucket, Prefix=args.prefix):
-    for obj in page.get('Contents', []):
-        key = obj['Key']
-        if args.test:
-            print(key)
-            continue
-        response = s3.get_object(Bucket=args.bucket, Key=key)
+keys = [
+    obj['Key']
+    for page in paginator.paginate(Bucket=args.bucket, Prefix=args.prefix)
+    for obj in page.get('Contents', [])
+]
+
+if args.test:
+    for key in keys:
+        print(key)
+else:
+    def hash_key(key):
+        client = boto3.client('s3')
+        response = client.get_object(Bucket=args.bucket, Key=key)
         sha256 = hashlib.sha256()
         for chunk in response['Body'].iter_chunks(chunk_size=65536):
             sha256.update(chunk)
-        digest = sha256.hexdigest()
-        line = f"{digest}  {key}"
-        lines.append(line)
-        print(line)
+        return f"{sha256.hexdigest()}  {key}"
 
-if not args.test:
+    with ThreadPoolExecutor(max_workers=args.workers) as executor:
+        lines = list(executor.map(hash_key, keys))
+
+    for line in lines:
+        print(line)
     open(args.file, 'w').write('\n'.join(lines))
